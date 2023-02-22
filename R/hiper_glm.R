1e1a93f9ee220fdc6f0f4ef067319f51c539acc5
Shengtao Wang <wangst224@gmail.com>
2023-02-22 15:02:16 -0500

    Implement MLE finders for hiper_glm linear

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -1,5 +1,5 @@
 #' @export
-hiper_glm = function(design, outcome, model = "linear"){
+hiper_glm = function(design, outcome, model = "linear", option = list()){
 
     supported_model = c("linear", "logit")
 
@@ -7,10 +7,19 @@ hiper_glm = function(design, outcome, model = "linear"){
         stop(sprintf("The model %s is not supported.", model))
     }
 
-    warning("'hiper_glm' is yet to be implemented")
-
     hglm_out = list()
-    class(hglm_out) = "hglm"
+    class(hglm_out) = 'hglm'
+
+    if (model == 'linear'){
+
+        if (is.null(option$mle_finder == "pseudo_inv")){
+            hglm_out$coefficients = find_mle_linear_pseudo_inv(design, outcome)
+        }
+
+        else {
+            hglm_out$coefficients = find_mle_linear_bfgs(design, outcome)
+        }
+    }
 
     return(hglm_out)
 }
