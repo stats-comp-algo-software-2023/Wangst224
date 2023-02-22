5803f879907a5b9193280004f002af66edce7473
Shengtao Wang <wangst224@gmail.com>
2023-02-22 13:50:48 -0500

    Set up function skeleton

---
+++ R/model_output.R
@@ -0,0 +1,16 @@
+#' @export
+coef.hglm = function(hglm_out){
+    warning("To be implemented")
+}
+
+
+#' @export
+vcov.hglm = function(hglm_out){
+    warning("To be implemented")
+}
+
+
+#' @export
+print.hglm = function(hglm_out){
+    cat("Output for hiperGLM")
+}
